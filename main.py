ecd664488f073429d7c86bd50f3d908ff1672396
Biri-Said <akmansaitbera@gmail.com>
2024-11-19 17:54:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 import requests
 from ..Fonksiyonlar import bot_mantik
 
-intents = discord.Intents.default
+intents = discord.Intents.default()
 
 intents.message_content = True
 
